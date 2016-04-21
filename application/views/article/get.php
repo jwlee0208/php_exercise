0289969e63d24dfc24196da7cd98b876620d9185
Jinwon Lee <jwlee0208@gmail.com>
2016-04-21 12:04:07 +0900

    Helper

--- application/views/article/get.php
+++ application/views/article/get.php
@@ -1,7 +1,9 @@
 
 <div class="span10">
-<article>
-	<h1><?=$article -> title?></h1>
-	<div><?=$article -> content?></div>
-</article>
+	<article>
+		<h1><?=$article -> title?></h1>
+		<div><?=$article -> content?></div>
+		<div><?=kdate($article -> create_date)?></div>
+		<?=auto_link($article -> content)?>
+	</article>
 </div>
